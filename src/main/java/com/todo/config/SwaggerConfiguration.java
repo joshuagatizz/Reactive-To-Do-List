5c6f055e44dcd9b02d1d3061fed4a683011237c3
Joshua Erlangga Sakti <57672096+joshuagatizz@users.noreply.github.com>
2022-07-09 23:23:39 +0700

    Update SwaggerConfiguration.java

--- src/main/java/com/todo/config/SwaggerConfiguration.java
+++ src/main/java/com/todo/config/SwaggerConfiguration.java
@@ -12,10 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * @author Suprayan Yapura
- * @since 0.0.1
- */
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration implements WebFluxConfigurer {
